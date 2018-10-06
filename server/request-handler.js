ff8e43122f657c34f1bb832c2d19a807de97b4a4
sveta0112 <sveta0112@hotmail.com>
2018-10-06 14:19:52 -0700

    10 tests are passing

--- server/request-handler.js
+++ server/request-handler.js
@@ -18,6 +18,23 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+// These headers will allow Cross-Origin Resource Sharing (CORS).
+// This code allows this server to talk to websites that
+// are on different domains, for instance, your chat client.
+// var Messages = {
+  
+//   NewMessage: function(userName, messageText, roomName = 'lobby',id) {
+    
+//     this.username = userName;
+//     this.text = messageText;
+//     this.roomname = roomName;
+//     this.id = `message_${this.storage.length + 1}`;
+    
+//   },
+  
+//   storage: []
+// };
+var storage = [];
 
 var requestHandler = function(request, response) {
   var statusCode = 200;
@@ -26,12 +43,62 @@ var requestHandler = function(request, response) {
   var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
-  //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-
   headers['Content-Type'] = 'application/json';
 
+
+
+  // Do some basic logging.
+  //
+  // Adding more logging to your server can be an easy way to get passive
+  // debugging help, but you should always be careful about leaving stray
+  // console.logs in your code.
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  console.log(request);
+  // The outgoing status.
+
+
+
+  if (request.method === 'POST' && request.url === '/classes/messages') {
+    //response.writeHead(statusCode, headers);
+    response.statusCode = 201;
+    let body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+      Messages.storage.push(chunk);
+    }).on('end', () => { 
+      body = Buffer.concat(body).toString();
+      storage.push(JSON.parse(body));
+      //Messages.storage = Messages.storage.concat(body);
+      //response.end(JSON.stringify(Messages.storage[Messages.storage.indexOf('message_' + Messages.storage.length + 1 + '')]));
+      // response.end(body);
+    });
+    response.end();
+    
+    // Should we concat Messages.storage to Buffer?
+
+    // the response has a
+
+
+
+    // response.on('error', (err) => {
+    //   console.error(err);
+    // });
+    //
+    // request.pipe(response);
+    // console.log(response);
+  } else if (request.method === 'GET' && request.url === '/classes/messages') {
+    //console.log(request);
+    response.writeHead(statusCode, headers);
+    let myObj = {results: storage};
+    
+    //myObj.results = Messages.storage;
+    response.end(JSON.stringify(myObj));
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
   // Make sure to always call response.end() - Node may not send
@@ -49,45 +116,8 @@ var requestHandler = function(request, response) {
   //
   // Documentation for both request and response can be found in the HTTP section at
   // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-
-
-  let body = [];
-  request.on('error', (err) => { 
-    console.error(err); 
-    response.statusCode = 400;
-    response.end();
-  });
-  response.on('error', (err) => {
-    console.error(err);
-  });
-
-  if (request.method === 'POST' && request.url === 'http://127.0.0.1:3000/classes/messages') {
-    request.pipe(response);
-  } else {
-    response.statusCode = 404;
-    response.end();
-  }
-
-  if (request.method === 'GET' && request.url === 'http://127.0.0.1:3000/classes/messages') {
-    request.pipe(response);
-  } else {
-    response.statusCode = 404;
-    response.end();
-  }
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
 //
 // Your chat client is running from a url like file://your/chat/client/index.html,
 // which is considered a different domain.
--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -60,6 +60,7 @@ describe('server', function() {
       // Now if we request the log, that message we posted should be there:
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messages = JSON.parse(body).results;
+        console.log('This is my console log', messages[0].username);
         expect(messages[0].username).to.equal('Jono');
         expect(messages[0].text).to.equal('Do my bidding!');
         done();
